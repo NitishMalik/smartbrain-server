adac4dc6edabc5f2a04e154e20f07d149eb8cc47
Nitish Malik <nitmalik@publicisgroupe.net>
2020-07-08 11:24:05 +0530

    adding redis and token authorization adding lambda serverless code sample

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,9 +1,44 @@
-const handleSignin = (db, bcrypt) => (req, res) => {
+const jwt = require('jsonwebtoken');
+
+// Redis Setup
+const redis = require('redis');
+//Update host for production
+const redisClient = redis.createClient({ host: '127.0.0.1' })
+
+const setToken = (key, value) => Promise.resolve(redisClient.set(key, value));
+const signToken = (userDetail) => {
+  const jwtPayload = { userDetail };
+  return jwt.sign(jwtPayload, 'JWT_SECRET', { expiresIn: '2 days' });
+}
+
+const createSession = (user) => {
+  const { email, id } = user;
+  const token = signToken(email);
+  console.log(token);
+  return setToken(token, id)
+    .then(() => {
+      return { success: 'true', userId: id, token, user }
+    })
+    .catch(err => console.log(err));
+}
+
+const getAuthTokenId = (req, res) => {
+  const { authorization } = req;
+  return redisClient.get(authorization, (err, response) => {
+    if (err || !response) {
+      return res.status(401).send('Unauthorized');
+    }
+    return res.json({ id: response });
+  })
+}
+
+const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).json('incorrect form submission');
+    return Promise.reject('incorrect form submission');
   }
-  db.select('email', 'hash').from('login')
+  console.log("I am here");
+  return db.select('email', 'hash').from('login')
     .where('email', '=', email)
     .then(data => {
       const isValid = bcrypt.compareSync(password, data[0].hash);
@@ -11,16 +46,27 @@ const handleSignin = (db, bcrypt) => (req, res) => {
         return db.select('*').from('users')
           .where('email', '=', email)
           .then(user => {
-            res.json(user[0])
+            console.log(user);
+            return user[0]
           })
-          .catch(err => res.status(400).json('unable to get user'))
+          .catch(err => Promise.reject('unable to get user'))
       } else {
-        res.status(400).json('wrong credentials')
+        return Promise.reject('wrong credentials')
       }
     })
-    .catch(err => res.status(400).json('wrong credentials'))
+    .catch(err => Promise.reject('wrong credentials'))
 }
 
+const signinAuthentication = (db, bcrypt) => (req, res) => {
+  const { authorization } = req.headers;
+  return authorization ? getAuthTokenId(req, res)
+    : handleSignin(db, bcrypt, req, res)
+      .then(data => data.id && data.email ? createSession(data) : Promise.reject(data))
+      .then(session => res.json(session))
+      .catch(err => res.status(400).json(err));
+
+}
 module.exports = {
-  handleSignin: handleSignin
+  signinAuthentication,
+  redisClient
 }
